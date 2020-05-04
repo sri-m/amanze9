945c919d7486b2680c4e3365d14245e9cf936ea6
sri-m <srinivaspkl@gmail.com>
2020-05-04 19:57:34 +0530

    one

---
+++ objects-types.rb
@@ -0,0 +1,31 @@
+#5 types variables: local, constant, global, instance, class
+name = "srinivas"    #string object
+Name = "M Srinivas"
+
+$age = 45     #integer object
+@salary = 50000.00 # float object
+
+#Object types: 8 types of Objects
+arr = [:srinivas, :Hyderabad, 45]    #array object: declare strings, integers, floats, array in array,
+			#nil, symbol, hash
+#An array is collection of different objects
+arr1 = ["srinvias", "hyderabad", "software", "tester", 60000.00, nil]
+puts arr1[-3]
+name = "Saritha"   #1kb 
+name1 = :Saritha   #0.5kb
+#use symbol anywhere string place
+puts name, name1
+#Hash is a collection of keys and values pairs
+h1 = {:name => "Ch Saritha", job: "Software Tester", loc: "USA"}
+puts h1[:name]
+puts h1[:job]
+puts h1[:loc]
+
+
+
+
+
+
+
+
+
